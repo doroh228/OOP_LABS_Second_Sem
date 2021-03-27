08b3c3d28c6528af59238080fe50ee5397896a86
doroh228 <doroh.2001@gmail.com>
2021-03-27 10:30:40 +0300

    created simple calculator

--- Tests/Learn_WPF/Learn_WPF/MainWindow.xaml.cs
+++ Tests/Learn_WPF/Learn_WPF/MainWindow.xaml.cs
@@ -20,9 +20,92 @@ namespace Learn_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        string leftop = string.Empty;
+        string operation = string.Empty;
+        string rightop = string.Empty;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            foreach (UIElement c in LayoutRoot.Children)
+            {
+                if(c is Button)
+                {
+                    ((Button)c).Click += Button_Click;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string s = (string)((Button)e.OriginalSource).Content;
+
+            textBlock.Text += s;
+
+            int num;
+
+            bool result = Int32.TryParse(s, out num);
+            if (result)
+            {
+                if (string.IsNullOrEmpty(operation))
+                {
+                    leftop += s;
+                }
+                else
+                {
+                    rightop += s;
+                }
+            }
+            else
+            {
+                if (s == "=")
+                {
+                    Update_RightOp();
+                    textBlock.Text += rightop;
+                    operation = string.Empty;
+
+                }
+                else if(s == "CLEAR")
+                {
+                    leftop = string.Empty;
+                    rightop = string.Empty;
+                    operation = string.Empty;
+                    textBlock.Text = string.Empty;
+                }
+                else
+                {
+                    if (rightop != "")
+                    {
+                        Update_RightOp();
+                        leftop = rightop;
+                        rightop = "";
+                    }
+                    operation = s;
+                }
+            }
+        }
+
+        private void Update_RightOp()
+        {
+            int num1 = Int32.Parse(leftop);
+            int num2 = Int32.Parse(rightop);
+            // И выполняем операцию
+            switch (operation)
+            {
+                case "+":
+                    rightop = (num1 + num2).ToString();
+                    break;
+                case "-":
+                    rightop = (num1 - num2).ToString();
+                    break;
+                case "*":
+                    rightop = (num1 * num2).ToString();
+                    break;
+                case "/":
+                    rightop = (num1 / num2).ToString();
+                    break;
+            }
         }
     }
 }
