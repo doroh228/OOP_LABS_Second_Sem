91d2cec155a3c788b0b2aa16d9e11466bba73d90
doroh228 <doroh.2001@gmail.com>
2021-03-27 23:03:09 +0300

    testing works VM mane window

--- LABA6_7_OOP/MyShop/MyShop/ViewModels/MainWindowViewModel.cs
+++ LABA6_7_OOP/MyShop/MyShop/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,23 @@
 ﻿
 namespace MyShop.ViewModels.Base
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        #region Title of Window
+        private string _Title = "Taxi-LR";
+
+        /// <summary>Title Window</summary>
+        public string Title
+        {
+            get => _Title;
+            //set
+            //{
+            //    if (Equals(_Title, value)) return;
+            //    _Title = value;
+            //    OnPropertyChanged();
+            //}
+            set => Set(ref _Title, value);
+        }
+        #endregion
     }
 }
