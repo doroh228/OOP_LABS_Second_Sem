27d584158b8ee6ec51cc93a5172d2b0a52609ed4
doroh228 <doroh.2001@gmail.com>
2021-03-27 21:46:22 +0300

    base class VM

---
+++ LABA6_7_OOP/MyShop/MyShop/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MyShop.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)// Мы можем вызывать не указывая в параметр имя свойства
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)//[referance, new value, propertyName]- разрешает менять дургие свойства при смене одного из свойств
+        {
+            if (Equals(field, value)) return false;
+
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
