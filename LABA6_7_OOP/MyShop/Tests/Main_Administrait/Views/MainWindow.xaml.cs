f1d0d30e36985f99cf1bc152bd6376b5d3d68cbb
doroh228 <doroh.2001@gmail.com>
2021-03-31 11:20:35 +0300

    started create a Main Window

---
+++ LABA6_7_OOP/MyShop/Tests/Main_Administrait/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Main_Administrait
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
