6629300bdbebd77d4d3cb7303dcc6bdaa253f9ab
doroh228 <doroh.2001@gmail.com>
2021-04-01 15:05:35 +0300

    edirt colors in LogIn Window

--- LABA6_7_OOP/MyShop/Tests/MyShopConsole/Program.cs
+++ LABA6_7_OOP/MyShop/Tests/MyShopConsole/Program.cs
@@ -4,9 +4,9 @@ namespace MyShopConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
